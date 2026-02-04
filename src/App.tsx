12a2ca91ace03ad373cd03e6fb4fba642eab6d05
Renato Takaoka <renato.takaoka13@gmail.com>
2026-02-04 15:03:24 -0300

    Criando componentes iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 
+import { Heading } from "./components/Heading";
+import { Container } from "./components/Container";
+
 export function App() {
   return (
-    <div className='App'>
-      <h1>React + Vite + TypeScript</h1>
-    </div>
+    <>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+      <Container>
+        <section>Menu</section>
+      </Container>
+    </>
   );
 }
