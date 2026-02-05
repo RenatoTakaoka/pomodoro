7bb6d682a8d1d2813e449b7342d9761468b50156
Renato Takaoka <renato.takaoka13@gmail.com>
2026-02-05 14:29:52 -0300

    Melhorado formulario do main

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,12 +2,61 @@ import { PlayCircle } from "lucide-react";
 import { Button } from "../Button";
 import { Cycles } from "../Cycles";
 import { Input } from "../Input";
+import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (!taskNameInput.current) {
+      return;
+    }
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert("Por favor, insira o nome da tarefa.");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: "workTime",
+    };
+
+    setState((prevState) => ({
+      ...prevState,
+      activeTask: newTask,
+      currentCycle: 1,
+      secondsRemaining: newTask.duration * 60,
+      formattedSecondsRemaining: `${String(newTask.duration).padStart(2, "0")}:00`,
+      tasks: [...prevState.tasks, newTask],
+    }));
+
+    taskNameInput.current.value = "";
+  }
+
   return (
-    <form className="form" action="">
+    <form onSubmit={handleCreateNewTask} className="form" action="">
       <div className="formRow">
-        <Input id="task" type="text" labelText="text" placeholder="Sei la" />
+        <Input
+          id="task"
+          type="text"
+          labelText="text"
+          placeholder="Sei la"
+          ref={taskNameInput}
+        />
       </div>
 
       <div className="formRow">
