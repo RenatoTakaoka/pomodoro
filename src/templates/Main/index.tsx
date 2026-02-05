0fe97e1e9cc92cca12d8de22586847099ea987aa
Renato Takaoka <renato.takaoka13@gmail.com>
2026-02-05 12:18:41 -0300

    Refatorado pagina de home e adicionado not found page

---
+++ src/templates/Main/index.tsx
@@ -0,0 +1,28 @@
+import { Container } from "../../components/Container";
+import { Logo } from "../../components/Logo";
+import { Menu } from "../../components/Menu";
+import { Footer } from "../../components/Footer";
+
+type MainProps = {
+  children: React.ReactNode;
+};
+
+export function MainTemplate({ children }: MainProps) {
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      {children}
+
+      <Container>
+        <Footer />
+      </Container>
+    </>
+  );
+}
